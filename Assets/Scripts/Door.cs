328c1f7277d0dd214deaf3c3316c43fc4e8edec0
Erick Simões <erick.simoes@live.com>
2017-07-09 22:19:43 -0300

    feat: Add door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	void Start() {
+		GetComponent<Animator>().StartPlayback();
+	}
+
+	public void OpenDoor() {
+		GetComponent<Animator>().StopPlayback();
+	}
+
+}
