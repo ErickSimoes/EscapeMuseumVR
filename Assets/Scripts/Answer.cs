61f284f35de3ddcde7b9a793337826271ded2df7
Erick Simões <erick.simoes@live.com>
2017-07-12 19:22:21 -0300

    feat: Add isCorrect and nextScene parameters

--- Assets/Scripts/Answer.cs
+++ Assets/Scripts/Answer.cs
@@ -1,21 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Answer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public string nextScene;
 
-	public void LoadNextScene() {
-		SceneManager.LoadScene("BaseScene");
+	public void LoadNextScene(bool isTheCorrect) {
+		if (isTheCorrect) {
+			SceneManager.LoadScene(nextScene);
+		} else {
+			//TODO: feedback  for error
+		}
 	}
 }
