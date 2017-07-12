c19a1783895f63abf648d2d07c2d3e170915ba08
Erick Simões <erick.simoes@live.com>
2017-07-11 21:05:00 -0300

    feat: Start instructions

---
+++ Assets/Scripts/UIStartInstructions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIStartInstructions : MonoBehaviour {
+
+	private Text welcomeText;
+	private Button buttonText;
+
+	private List<string> instructions = new List<string>();
+	private int i = 0;
+
+	void Start () {
+		welcomeText = GetComponentInChildren<Text>();
+		buttonText = GetComponentInChildren<Button>();
+
+		instructions.Add("Enter the museum and\nlearn about great artists");
+		instructions.Add("Look at the paintings\nand answer the questions");
+		instructions.Add("Good luck\nand enjoy!");
+
+
+	}
+
+	public void OnClick() {
+		if (i < instructions.Count) {
+			welcomeText.text = instructions[i++];
+		}
+
+		if (i == instructions.Count) {
+			buttonText.GetComponentInChildren<Text>().text = "START";
+			i++;
+		} else if (i > instructions.Count) {
+			GetComponentInParent<Canvas>().enabled = false;
+		}
+	}
+}
