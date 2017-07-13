8b3c5a077097348e2a5b391cdbfcde976058f551
Erick Simões <erick.simoes@live.com>
2017-07-12 21:52:02 -0300

    feat: Fade and out
    When the answer is correct, the light goes out and when it lights
    up, the player is in another scene.

--- Assets/Scripts/Answer.cs
+++ Assets/Scripts/Answer.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class Answer : MonoBehaviour {
@@ -7,9 +8,21 @@ public class Answer : MonoBehaviour {
 
 	public void LoadNextScene(bool isTheCorrect) {
 		if (isTheCorrect) {
-			SceneManager.LoadScene(nextScene);
+			StartCoroutine(FadeAndOut());
+			print("Acabou");
 		} else {
 			//TODO: feedback  for error
 		}
+	}
+
+	IEnumerator FadeAndOut() {
+		for (float i = 0; i < 1; i = i + 0.01f) {
+			print(i);
+			FadePanel.FadeOut(i);
+			yield return null;
+		}
+
+		SceneManager.LoadScene(nextScene);
+		yield return new WaitForSeconds(3f);
 	}
 }
